d3dbc1fc5a4a1fe5c0983f617244a4ad8ee303ed
thuytrang <thainguyenthuytrang1041999@gmail.com>
2020-01-07 21:17:59 +0700

    lesson01-code-07.01.2020

---
+++ lesson01-setup-enviroment/src/main/java/ThuyTrang/lesson01_setup_enviroment/App.java
@@ -0,0 +1,11 @@
+package ThuyTrang.lesson01_setup_enviroment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
